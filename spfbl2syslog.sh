c624e39ffc7ee9ae30a597f3c8451defbb260c0e
Noilson Caio T. de Araújo <caiogore@gmail.com>
2017-01-12 08:36:06 -0300

    Correção em parâmetro
    linha 22 variável $_facility declarada com erro de sintaxe

--- spfbl2syslog.sh
+++ spfbl2syslog.sh
@@ -19,7 +19,7 @@ tail -f "$_logpath"spfbl."$_logdate".log |
 while read -r line
 do
    newline="$(echo "$line" | awk '{$1=$2=""; print $0}')"
-   logger -p "$facility="."$_level" -t SPFBL "$newline"
+   logger -p "$_facility"."$_level" -t SPFBL "$newline"
 done
 #
 #

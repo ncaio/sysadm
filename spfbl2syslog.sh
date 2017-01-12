4d87ee3f6d7a319dc51a948edabbee0154053344
Noilson Caio T. de Araújo <caiogore@gmail.com>
2017-01-11 21:57:01 -0300

    Melhorias do script
    Criação das variáveis para o facility e level 
    Breve cabeçalho descritivo

--- spfbl2syslog.sh
+++ spfbl2syslog.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #
+#       ESTE SCRIPT REALIZA A LEITURA DOS LOGs DO SPFBL 
+#       E TRANSPORTA PARA O SYSLOG VIA COMMANDO logger
+#       TOPICO: https://groups.google.com/forum/#!topic/spfbl/Xo8dbYKq4IU
 #       Noilson Caio 10-01-2007
 #
 _logpath="/var/log/spfbl/"
 _logdate="$(date +%Y-%m-%d)"
+_facility="local3"
+_level="notice"
 #
 #
 #
@@ -14,7 +19,7 @@ tail -f "$_logpath"spfbl."$_logdate".log |
 while read -r line
 do
    newline="$(echo "$line" | awk '{$1=$2=""; print $0}')"
-   logger -p local3.notice -t SPFBL "$newline"
+   logger -p "$facility="."$_level" -t SPFBL "$newline"
 done
 #
 #
